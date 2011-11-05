86789a5017d9e2942387dc5cdc82e35e7c76da21
Jose Luis <joseluisalcalaramos@Jose-Luis-Alcala-Ramoss-Mac.local>
2011-11-05 14:06:51 +0100

    Second commit

---
+++ app/Util/maxmind/geoip/Region.java
@@ -0,0 +1,8 @@
+package Util.maxmind.geoip;
+
+public class Region{
+  public String countryCode;
+  public String countryName;
+  public String region;
+}
+
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,14 @@
 package controllers;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 import Util.Utils;
+import Util.maxmind.geoip.Location;
+import Util.maxmind.geoip.LookupService;
+import Util.maxmind.geoip.regionName;
+import Util.maxmind.geoip.timeZone;
 
 
 
@@ -20,9 +25,34 @@ public class Application extends Controller {
        esta no debería devolver nada por el momento 
      */
     
-	public static void index() {
+	public static void index() throws IOException {
+		
 		String ipRemote=Utils.getIpRemote(request);
 		Logger.info("ip--"+ipRemote);
+		String rutaFichero = "lib/GeoLiteCity.dat";
+		rutaFichero.replaceAll("/", File.separator);
+		 
+		 LookupService cl = new LookupService(rutaFichero,
+					LookupService.GEOIP_MEMORY_CACHE );
+         Location l1 = cl.getLocation("213.52.50.8");
+         Location l2 = cl.getLocation(ipRemote);
+         Logger.info("countryCode: " + l2.countryCode +
+                            "\n countryName: " + l2.countryName +
+                            "\n region: " + l2.region +
+                            "\n regionName: " + regionName.regionNameByCode(l2.countryCode, l2.region) +
+                            "\n city: " + l2.city +
+                            "\n postalCode: " + l2.postalCode +
+                            "\n latitude: " + l2.latitude +
+                            "\n longitude: " + l2.longitude +
+                            "\n distance: " + l2.distance(l1) +
+                            "\n distance: " + l1.distance(l2) + 
+			       "\n metro code: " + l2.metro_code +
+			       "\n area code: " + l2.area_code +
+                            "\n timezone: " + timeZone.timeZoneByCountryAndRegion(l2.countryCode, l2.region));
+
+	    cl.close();
+
+		
         render();
     }
 
